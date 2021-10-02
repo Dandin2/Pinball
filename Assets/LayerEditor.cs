196e1fb0ba3488940ea98a0b96213a4a16c49f5e
Dandin2 <castel2424@gmail.com>
2021-10-02 15:07:50 -0400

    test commit

--- Assets/LayerEditor.cs
+++ Assets/LayerEditor.cs
@@ -1,21 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class LayerEditor : MonoBehaviour
 {
-    public List<GameObject> aboveGameObjects;
-    public List<GameObject> belowGameObjects;
-
+    public List<LayerChangeInfo> ConditionalChanges;
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if(collision.name == "Ball")
+        if (collision.name == "Ball")
         {
-            if(collision.transform.position.x > transform.position.x)
+            LayerChangeInfo lci = null;
+            //Check the speed of the ball,  Assume that whichever direction is "fastest" is the direction you exited out of.  IDK why
+            if (Math.Abs(collision.GetComponent<Rigidbody2D>().velocity.x) > Math.Abs(collision.GetComponent<Rigidbody2D>().velocity.y))//collision.transform.position.x > transform.position.x)
             {
+                if (collision.GetComponent<Rigidbody2D>().velocity.x > 0)
+                    lci = ConditionalChanges.Where(x => x.exitPosition == PositionTriggerType.Right).FirstOrDefault();
+                if (collision.GetComponent<Rigidbody2D>().velocity.x < 0)
+                    lci = ConditionalChanges.Where(x => x.exitPosition == PositionTriggerType.Left).FirstOrDefault();
+            }
+            else
+            {
+                if (collision.GetComponent<Rigidbody2D>().velocity.y > 0)
+                    lci = ConditionalChanges.Where(x => x.exitPosition == PositionTriggerType.Above).FirstOrDefault();
+                if (collision.GetComponent<Rigidbody2D>().velocity.y < 0)
+                    lci = ConditionalChanges.Where(x => x.exitPosition == PositionTriggerType.Below).FirstOrDefault();
+            }
 
+            if (lci != null)
+            {
+                foreach (GameObject go in lci.objectsToDisableCollision)
+                    go.GetComponent<PolygonCollider2D>().enabled = false;
+                foreach (GameObject go in lci.objectsToEnableCollision)
+                    go.GetComponent<PolygonCollider2D>().enabled = true;
             }
         }
     }
+}
+
+[Serializable]
+public class LayerChangeInfo
+{
+    public PositionTriggerType exitPosition;
+    public List<GameObject> objectsToDisableCollision;
+    public List<GameObject> objectsToEnableCollision;
+}
+
+public enum PositionTriggerType
+{
+    ExitAbove,
+    ExitBelow,
+    ExitLeft,
+    ExitRight,
+    EnterAbove,
+    EnterBelow,
+    EnterLeft,
+    EnterRight
 }
