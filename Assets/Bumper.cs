8b0008ce747665752fc5a65e0fab9a0c8bead9a4
Eric Oudin <Eric_Oudin29@yahoo.com>
2021-10-02 17:23:52 -0400

    updated bumper.

--- Assets/Bumper.cs
+++ Assets/Bumper.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class Bumper : MonoBehaviour
 {
 	public float force;
+	public Animator anim;
+
+
 	void OnCollisionEnter2D(Collision2D collision)
 	{
+		anim.SetTrigger("Hit");
 		var contactObject = collision.gameObject;
 		var direction = (contactObject.transform.position - transform.position).normalized;
 		contactObject.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
