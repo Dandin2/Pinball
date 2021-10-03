95fd6e7452a5d28311d394877119c7fda2baffcf
Eric Oudin <Eric_Oudin29@yahoo.com>
2021-10-03 19:31:09 -0400

    side plungers

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -12,7 +12,6 @@ public class Ball : MonoBehaviour
 
 	private void Start()
 	{
-		Debug.Log(transform.position);
 		startingPosition = transform.position;
 		audioSource = GetComponent<AudioSource>();
 	}
