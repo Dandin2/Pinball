fd1ee619486b45d5e510e4bcb62a9f428f76df90
Eric Oudin <Eric_Oudin29@yahoo.com>
2021-10-02 20:21:17 -0400

    triangle bumper.

--- Assets/Bumper.cs
+++ Assets/Bumper.cs
@@ -6,13 +6,20 @@ public class Bumper : MonoBehaviour
 {
 	public float force;
 	public Animator anim;
-
+	public Transform directionalTransform;
 
 	void OnCollisionEnter2D(Collision2D collision)
 	{
 		anim.SetTrigger("Hit");
-		var contactObject = collision.gameObject;
-		var direction = (contactObject.transform.position - transform.position).normalized;
-		contactObject.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
+		Vector3 direction = Vector3.zero;
+		if (directionalTransform != null)
+		{
+			direction = (transform.position - directionalTransform.position).normalized;
+		}
+		else
+		{
+			direction = (collision.gameObject.transform.position - transform.position).normalized;
+		}
+		collision.gameObject.GetComponent<Rigidbody2D>().AddForce(direction * force, ForceMode2D.Impulse);
 	}
 }
