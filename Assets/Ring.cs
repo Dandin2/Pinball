19250ceab7a14aed72594af6cb9bd1a39c9b4d48
Eric Oudin <Eric_Oudin29@yahoo.com>
2021-10-03 23:28:35 -0400

    added ring to center of level.

---
+++ Assets/Ring.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ring : MonoBehaviour
+{
+    public KeyCode TiltLeft;
+    public KeyCode TiltRight;
+	public float LeftAngle;
+	public float RightAngle;
+
+	private State ringState = State.Center;
+	private Rigidbody2D _rb;
+	private float rotationSpeed = 5f;
+	private enum State
+	{
+		Left = 0,
+		Center = 1,
+		Right = 2
+	}
+
+	private void Awake()
+	{
+		_rb = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		if(Input.GetKeyDown(TiltLeft))
+		{
+			if (ringState != State.Left)
+				ringState--;
+		}
+		else if (Input.GetKeyDown(TiltRight))
+		{
+			if (ringState != State.Right)
+				ringState++;
+		}
+	}
+
+	private void FixedUpdate()
+	{
+		if(ringState == State.Left && transform.rotation.eulerAngles.z != LeftAngle)
+		{
+			var rotation = Mathf.Lerp(_rb.rotation, LeftAngle, rotationSpeed * Time.fixedDeltaTime);
+			if (LeftAngle - rotation < 1)
+			{
+				rotation = LeftAngle;
+			}
+			_rb.MoveRotation(rotation);
+		}
+		else if(ringState == State.Center && transform.rotation.eulerAngles.z != 0)
+		{
+			var rotation = Mathf.Lerp(_rb.rotation, 0, rotationSpeed * Time.fixedDeltaTime);
+			if (rotation < 1 && rotation > -1)
+			{
+				rotation = 0;
+			}
+			_rb.MoveRotation(rotation);
+		}
+		else if(ringState == State.Right && transform.rotation.eulerAngles.z != RightAngle)
+		{
+			var rotation = Mathf.Lerp(_rb.rotation, RightAngle, rotationSpeed * Time.fixedDeltaTime);
+			if(RightAngle - rotation > -1)
+			{
+				rotation = RightAngle;
+			}
+			_rb.MoveRotation(rotation);
+		}
+	}
+}
